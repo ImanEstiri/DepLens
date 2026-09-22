a035ca1382fed026b1d740b7b41ff5da1b0bdbe9
Iman Estiri <is812818@gmail.com>
2026-09-22 13:21:29 +0330

    core - file system service added

---
+++ src/Core/ImanSoftware.DepLens.Core/Implementation/FileSystem.cs
@@ -0,0 +1,13 @@
+﻿using ImanSoftware.DepLens.Abstractions.Services;
+
+namespace ImanSoftware.DepLens.Core.Implementation;
+
+internal sealed class PhysicalFileSystem : IFileSystem
+{
+    public bool FileExists(string path) => File.Exists(path);
+    public bool DirectoryExists(string path) => Directory.Exists(path);
+    public Task<string> ReadAllTextAsync(string path, CancellationToken ct)
+        => File.ReadAllTextAsync(path, ct);
+    public IEnumerable<string> EnumerateFiles(string rootPath, string pattern)
+        => Directory.EnumerateFiles(rootPath, pattern, SearchOption.AllDirectories);
+}
