5140eeb15b0a136aa9d8bf6368a9fee8ceca47db
Iman Estiri <is812818@gmail.com>
2026-09-22 13:21:13 +0330

    abstraction - file storage interface added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Services/IFileSystem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ImanSoftware.DepLens.Abstractions.Services;
+
+public interface IFileSystem : IService
+{ 
+    bool FileExists(string path);
+    bool DirectoryExists(string path);
+    Task<string> ReadAllTextAsync(string path, CancellationToken ct = default);
+    IEnumerable<string> EnumerateFiles(string rootPath, string searchPattern);
+}
