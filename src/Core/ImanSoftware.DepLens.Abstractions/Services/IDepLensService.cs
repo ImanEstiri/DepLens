1844141c9254f97ea181e04bd0da54eb148e2e69
Iman Estiri <is812818@gmail.com>
2026-09-22 10:03:08 +0330

    abstraction - main interface added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Services/IDepLensService.cs
@@ -0,0 +1,9 @@
+﻿
+using ImanSoftware.Outcomes;
+
+namespace ImanSoftware.DepLens.Abstractions.Services;
+
+public interface IDepLensService : IService
+{
+    Task<Outcome> Analyze(string path);
+}
