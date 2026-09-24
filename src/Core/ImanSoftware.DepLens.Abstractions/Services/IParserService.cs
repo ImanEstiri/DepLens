11f93dc31425503a714889e51a197bec16cea8ee
Iman Estiri <is812818@gmail.com>
2026-09-24 17:17:19 +0330

    abstraction - parser interface updated

--- src/Core/ImanSoftware.DepLens.Abstractions/Services/IParserService.cs
+++ src/Core/ImanSoftware.DepLens.Abstractions/Services/IParserService.cs
@@ -1,10 +1,10 @@
 ﻿
+using ImanSoftware.DepLens.Abstractions.Models;
 using ImanSoftware.Outcomes;
 
 namespace ImanSoftware.DepLens.Abstractions.Services;
 
 public interface IParserService : IService
 {
-    Task<Outcome> ParseSolution();
-    Task<Outcome> ParseProject();
+    Task<Outcome<ParsedFile>> ParseAsync(DiscoveredFile file);
 }
