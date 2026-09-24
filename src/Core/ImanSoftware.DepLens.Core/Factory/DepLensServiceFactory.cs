d02073dd3777e8b64e5c4db8ac75ecc2334db9fd
Iman Estiri <is812818@gmail.com>
2026-09-24 17:19:22 +0330

    core - deplense service factory added as a entry point

---
+++ src/Core/ImanSoftware.DepLens.Core/Factory/DepLensServiceFactory.cs
@@ -0,0 +1,9 @@
+﻿using ImanSoftware.DepLens.Abstractions.Services;
+using ImanSoftware.DepLens.Core.Implementation;
+
+namespace ImanSoftware.DepLens.Core.Factory;
+
+public static class DepLensServiceFactory
+{
+    public static IDepLensService Create() => new DepLensService();
+}
