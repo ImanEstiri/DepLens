56f974a70bc5cafe00e03d6b552e440f0a263c1a
Iman Estiri <is812818@gmail.com>
2026-09-24 17:18:06 +0330

    abstraction - directory scanner interface added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Services/IDirectoryScanner.cs
@@ -0,0 +1,10 @@
+﻿
+using ImanSoftware.DepLens.Abstractions.Models;
+using ImanSoftware.Outcomes;
+
+namespace ImanSoftware.DepLens.Abstractions.Services;
+
+public interface IDirectoryScanner : IService
+{
+    Task<Outcome<List<DiscoveredFile>>> InvestigateDirectoryAsync(string path);
+}
