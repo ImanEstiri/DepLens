e37722ba13f188fbfe161aa0e3d742ae84abc030
Iman Estiri <is812818@gmail.com>
2026-09-24 17:22:16 +0330

    abstraction - file type enum added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Models/Enums/FileType.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel;
+
+namespace ImanSoftware.DepLens.Abstractions.Models;
+
+public enum FileType
+{
+    None,
+
+    [Description("*.sln")]
+    SolutionClassic,      // .sln
+
+    [Description("*.slnx")]
+    SolutionXml,           // .slnx
+
+    [Description("*.csproj")]
+    Project,                // .csproj
+
+    [Description("Directory.Packages.props")]
+    DirectoryPackagesProps, // Directory.Packages.props
+
+    [Description("Directory.Build.props")]
+    DirectoryBuildProps     // Directory.Build.props 
+}
