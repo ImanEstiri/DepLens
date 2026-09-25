5121e2b672a9bc636fbb99b77724ef63b1c3643e
Iman Estiri <is812818@gmail.com>
2026-09-25 19:57:46 +0330

    abstraction - project oriented interface added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Services/IProjectOrienterService.cs
@@ -0,0 +1,12 @@
+﻿using ImanSoftware.DepLens.Abstractions.Models;
+using ImanSoftware.Outcomes;
+
+namespace ImanSoftware.DepLens.Abstractions.Services;
+
+public interface IProjectOrienterService : IService
+{
+    Task<Outcome<List<ProjectContext>>> Orient(
+        IReadOnlyList<DiscoveredFile> projectFiles,
+        IReadOnlyList<ParsedFile> parsedSolutions,
+        IReadOnlyList<ParsedFile> parsedPackagesProps);
+}
