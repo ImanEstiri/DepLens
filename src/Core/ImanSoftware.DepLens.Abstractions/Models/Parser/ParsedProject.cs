1844a85647850c853b9a9b097b630bc69490b31e
Iman Estiri <is812818@gmail.com>
2026-09-25 20:00:02 +0330

    abstraction - ParsedProject Model added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Models/Parser/ParsedProject.cs
@@ -0,0 +1,7 @@
+﻿namespace ImanSoftware.DepLens.Abstractions.Models;
+
+public sealed record ParsedProject(
+    IReadOnlyList<string> TargetFrameworks,
+    IReadOnlyList<RawProjectReference> ProjectReferences,
+    IReadOnlyList<PackageDependency> PackageReferences,
+    bool? ManagePackageVersionsCentrallyOverride) : IParsedContent;
