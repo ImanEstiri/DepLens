1880783b0a1c26e4d652685d583990e0c05672aa
Iman Estiri <is812818@gmail.com>
2026-09-25 19:59:00 +0330

    abstraction - PackageDependency model added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Models/Parser/PackageDependency.cs
@@ -0,0 +1,7 @@
+﻿// Models/Parser/PackageDependency.cs
+namespace ImanSoftware.DepLens.Abstractions.Models;
+
+public sealed record PackageDependency(
+    string Name,
+    string? ResolvedVersion,   
+    PackageVersionSourceType Source);
