f0b0f3c394109356f854b4a422785f3a2eaa89f2
Iman Estiri <is812818@gmail.com>
2026-09-25 20:00:22 +0330

    abstraction - ProjectContext Model added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Models/Parser/ProjectContext.cs
@@ -0,0 +1,7 @@
+﻿
+namespace ImanSoftware.DepLens.Abstractions.Models;
+
+public sealed record ProjectContext(
+    string ProjectFullPath,
+    IReadOnlyList<string> SolutionPaths,      
+    ParsedPackagesProps? CentralPackageVersions);
