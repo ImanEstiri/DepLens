69104a34082b7f709860fa6693a40ff1479e58f7
Iman Estiri <is812818@gmail.com>
2026-09-25 20:10:40 +0330

    abstraction - ParsedPackagesProps model added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Models/Parser/ParsedPackagesProps.cs
@@ -0,0 +1,6 @@
+﻿
+namespace ImanSoftware.DepLens.Abstractions.Models;
+
+public sealed record ParsedPackagesProps(
+    bool ManagePackageVersionsCentrally,
+    IReadOnlyDictionary<string, string> CentralPackageVersions) : IParsedContent;
