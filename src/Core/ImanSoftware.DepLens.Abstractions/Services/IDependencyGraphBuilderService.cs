1eff8d7425743bd15ba5c4cf6e3be46886c5f7cc
Iman Estiri <is812818@gmail.com>
2026-09-26 17:57:57 +0330

    abstraction - IDependencyGraphBuilderService added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Services/IDependencyGraphBuilderService.cs
@@ -0,0 +1,12 @@
+﻿using ImanSoftware.DepLens.Abstractions.Models;
+using ImanSoftware.Outcomes;
+
+namespace ImanSoftware.DepLens.Abstractions.Services;
+
+public interface IDependencyGraphBuilderService : IService
+{
+    Task<Outcome<List<ProjectDependencyReport>>> Build(
+        IReadOnlyList<ParsedFile> parsedProjects,
+        IReadOnlyList<ResolvedProjectReferences> resolvedReferences,
+        IReadOnlyList<ProjectContext> projectContexts);
+}
