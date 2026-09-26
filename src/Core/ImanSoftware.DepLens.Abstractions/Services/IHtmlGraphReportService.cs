914235292bbdf2830de1c4585d2f986cc966648d
Iman Estiri <is812818@gmail.com>
2026-09-26 17:58:22 +0330

    abstraction - IHtmlGraphReportService added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Services/IHtmlGraphReportService.cs
@@ -0,0 +1,12 @@
+﻿using ImanSoftware.DepLens.Abstractions.Models;
+using ImanSoftware.Outcomes;
+
+namespace ImanSoftware.DepLens.Abstractions.Services;
+
+public interface IHtmlGraphReportService : IService
+{
+    Task<Outcome<string>> GenerateAsync(
+        IReadOnlyList<ProjectDependencyReport> reports,
+        string outputDirectory,
+        string fileName = "dependency-graph.html");
+}
