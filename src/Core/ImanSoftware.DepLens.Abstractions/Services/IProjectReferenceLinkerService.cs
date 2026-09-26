f3f10af778bb605218e23a16777e0576870ca98e
Iman Estiri <is812818@gmail.com>
2026-09-26 17:58:49 +0330

    abstraction - IProjectReferenceLinkerService added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Services/IProjectReferenceLinkerService.cs
@@ -0,0 +1,9 @@
+﻿using ImanSoftware.DepLens.Abstractions.Models;
+using ImanSoftware.Outcomes;
+
+namespace ImanSoftware.DepLens.Abstractions.Services;
+
+public interface IProjectReferenceLinkerService : IService
+{
+    Task<Outcome<List<ResolvedProjectReferences>>> Link(IReadOnlyList<ParsedFile> parsedProjects);
+}
