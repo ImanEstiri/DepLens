d8b6cdc226bec55b8ee509df586669cc37e3e467
Iman Estiri <is812818@gmail.com>
2026-09-26 17:53:36 +0330

    abstraction - dependency model added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Models/Graph/Dependency.cs
@@ -0,0 +1,10 @@
+﻿namespace ImanSoftware.DepLens.Abstractions.Models;
+
+public sealed record Dependency(
+    string Name,
+    string? Version,
+    DependencyType Type,
+    DependencyScope Scope,
+    PackageVersionSourceType? VersionSource,
+    bool IsResolved,
+    string? TargetFullPath = null);
