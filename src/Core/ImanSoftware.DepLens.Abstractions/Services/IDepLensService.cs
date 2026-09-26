2a562c9917d8e8e6ab49c31b592c1905bdfada95
Iman Estiri <is812818@gmail.com>
2026-09-26 18:00:59 +0330

    abstraction - IDeplenseService returnType updated

--- src/Core/ImanSoftware.DepLens.Abstractions/Services/IDepLensService.cs
+++ src/Core/ImanSoftware.DepLens.Abstractions/Services/IDepLensService.cs
@@ -1,9 +1,10 @@
 ﻿
+using ImanSoftware.DepLens.Abstractions.Models;
 using ImanSoftware.Outcomes;
 
 namespace ImanSoftware.DepLens.Abstractions.Services;
 
 public interface IDepLensService : IService
 {
-    Task<Outcome> Analyze(string path);
+    Task<Outcome<List<ProjectDependencyReport>>> Analyze(string path);
 }
