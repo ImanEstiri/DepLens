36a9ca7d65ad797a444ab005bdfaca1f0be9eadf
Iman Estiri <is812818@gmail.com>
2026-09-26 17:55:25 +0330

    abstraction - ResolvedProjectReference model added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Models/Parser/ResolvedProjectReferences.cs
@@ -0,0 +1,5 @@
+﻿namespace ImanSoftware.DepLens.Abstractions.Models;
+
+public sealed record ResolvedProjectReferences(
+    string ProjectFullPath,
+    IReadOnlyList<ProjectReferenceLink> References);
