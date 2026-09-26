842b41ee27e8a526a9caf3202770ad0706dee066
Iman Estiri <is812818@gmail.com>
2026-09-26 17:54:32 +0330

    abstraction - ProjectDependencyReport model added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Models/Graph/ProjectDependencyReport.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace ImanSoftware.DepLens.Abstractions.Models;
+
+public sealed record ProjectDependencyReport(
+    string ProjectFullPath,
+    IReadOnlyList<string> SolutionPaths,
+    IReadOnlyList<Dependency> Dependencies);
