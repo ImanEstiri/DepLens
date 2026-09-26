d00c5388a24de533273441edac2d9506e238f783
Iman Estiri <is812818@gmail.com>
2026-09-26 17:54:58 +0330

    abstraction - ProjectReferenceLink model added

---
+++ src/Core/ImanSoftware.DepLens.Abstractions/Models/Parser/ProjectReferenceLink.cs
@@ -0,0 +1,7 @@
+﻿namespace ImanSoftware.DepLens.Abstractions.Models;
+
+public abstract record ProjectReferenceLink;
+
+public sealed record InternalProjectReference(string FullPath) : ProjectReferenceLink;
+
+public sealed record ExternalProjectReference(string RawPath) : ProjectReferenceLink;
